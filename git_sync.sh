327290feb730a8a867f9c24b54f8771555f06dd7
MisakiWang <misakiwang74@gmail.com>
2024-06-20 14:34:39 +0800

    dump update

--- git_sync.sh
+++ git_sync.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
-
-# 获取提交消息
 if [ -z "$1" ]
   then
     echo "No commit message supplied"
@@ -10,13 +8,9 @@ fi
 COMMIT_MESSAGE=$1
 
 git pull
-# 添加所有更改到暂存区
+
 git add .
-
-# 提交更改
 git commit -m "$COMMIT_MESSAGE"
-
-# 推送到远程仓库
 git push origin master
 
 echo "Changes have been committed and pushed to GitHub."
